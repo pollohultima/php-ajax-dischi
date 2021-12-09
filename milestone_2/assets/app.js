66187cdd848ba6a6ae85eda803d39aff06f53efe
pollohultima <matteo.polonio@gmail.com>
2021-12-09 16:56:40 +0100

    Bonus complete

--- milestone_2/assets/app.js
+++ milestone_2/assets/app.js
@@ -3,6 +3,9 @@ const app = new Vue({
     el: '#app',
     data: {
         discs: null,
+        genre: "default",
+        genres: [],
+        selector: "default",
     },
 
     mounted() {
@@ -11,7 +14,35 @@ const app = new Vue({
             .then(response => {
                 console.log(response);
                 this.discs = response.data
+
+                this.discs.forEach((disc) => {
+                    if (!this.genres.includes(disc.genre)) {
+                        this.genres.push(disc.genre);
+                    }
+                });
             })
 
-    }
+    },
+
+    computed: {
+        filterItems() {
+            if (this.genre === "default") {
+                return this.discs;
+            } else if (this.genre !== "default") {
+                return this.discs.filter(
+                    (disc) => disc.genre === this.genre);
+            }
+
+            return this.discs.filter(
+                (disc) => disc.genre === this.genre);
+        },
+    },
+
+    methods: {
+        pickGen(genre) {
+            this.genre = genre;
+        }
+    },
+
+
 })
--- milestone_2/index.php
+++ milestone_2/index.php
@@ -7,34 +7,43 @@ require __DIR__ . '/layouts/head.php';
 <!doctype html>
 <html lang="en">
 
-  <body>
+<body>
 
 
     <?php require __DIR__ . '/layouts/header.php';?>
 
     <main>
-        <div id="app">
+        <div id="app" class="position-relative">
+
+            <select class="form-select filter_gen position-absolute" aria-label="Default select example"
+                v-model="selector" style="width: 200px; top: -50px; left: 24px;" @change="pickGen(selector)">
+                <option value="default">Select a Genre</option>
+                <option v-for="genre in genres" :value="genre" :key="genre">
+                    {{ genre }}
+                </option>
+            </select>
 
             <div class="container-fluid gx-4 position-relative">
-               <div class="row gx-5 gy-4 justify-content-center">
+                <div class="row gx-5 gy-4 justify-content-center">
 
-                       <div v-for="disc in discs" class="col-2" :key="disc.title">
-                           <div class="card border-0 p-4">
-                               <img :src="disc.poster" alt="" class="mb-4" />
-                               <h5 class="text-center text-white">
-                               {{disc.title}}
-                               </h5>
-                               <p class="text-center text-muted m-0">{{disc.author}}</p>
-                               <p class="text-center text-muted m-0">{{disc.year}}</p>
-                           </div>
-                       </div>
+                    <div v-for="disc in filterItems" class="col-2" :key="disc.title">
+                        <div class="card border-0 p-4">
+                            <img :src="disc.poster" alt="" class="mb-4" />
+                            <h5 class="text-center text-white">
+                                {{disc.title}}
+                            </h5>
+                            <p class="text-center text-muted m-0">{{disc.author}}</p>
+                            <p class="text-center text-muted m-0">{{disc.year}}</p>
+                        </div>
+                    </div>
 
-               </div>
+                </div>
             </div>
 
         </div>
     </main>
 
     <?php require __DIR__ . '/layouts/footer.php';?>
-  </body>
+</body>
+
 </html>
